fd7b75d04391520ada9ffa4364ea30719da1adab
Jim Gripp <jimg4814@gmail.com>
2018-09-22 18:57:47 -0600

    Add project files.

---
+++ DjangoWeb/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\James\Documents\Visual Studio 2017\Projects\DjangoWeb\DjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
